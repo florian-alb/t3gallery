b5d289c4717cdccd24cbe9e1454c3b021b297d10
Florian <79457058+florian-alb@users.noreply.github.com>
2024-06-01 14:44:34 +0200

    more cleanup on modal

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3c449bf11d150be8975f153c18cdcdf9@o4507350390865920.ingest.de.sentry.io/4507350398533712",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,5 +4,14 @@ import {Modal} from "~/app/@modal/(.)img/[id]/modal";
 
 export default async function FullImagePageView(props: { id: number }) {
     const image = await getImage(props.id)
-    return <Image src={image.url} alt={image.name} height={480} width={480} className="h-96"/>
+    return (
+        <div className="flex w-full h-full min-w-0">
+            <div className="flex-shrink flex justify-center items-center">
+                <img src={image.url} alt={image.name} className="flex-shrink object-contain"/>
+            </div>
+            <div className="w-48 flex flex-col flex-shrink-0 border-l ">
+                <div className="text-xl font-bold">{image.name}</div>
+            </div>
+        </div>
+    )
 }
